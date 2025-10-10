10dd3ab2f414dc8731137c13d0cc2195ad3654fb
jiwonly <ysjjw2003@naver.com>
2025-10-10 14:49:30 +0900

    chore: initialize project

---
+++ src/main/java/finity/fini/FiniApplication.java
@@ -0,0 +1,13 @@
+package finity.fini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/finity/fini/FiniApplicationTests.java
@@ -0,0 +1,13 @@
+package finity.fini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
