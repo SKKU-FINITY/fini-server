74836ddef2416d495db2b170ea2e055ae4c9b7ce
jiwonly <ysjjw2003@naver.com>
2025-10-11 01:33:18 +0900

    feat: add user domain

---
+++ src/main/java/finity/fini/domain/User.java
@@ -0,0 +1,30 @@
+package finity.fini.domain;
+
+import finity.fini.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_id;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String username;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, length = 255)
+    private String email;
+}
