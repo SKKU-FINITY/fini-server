6aa8349d401b41496639c5ed4866cf1119745de3
jiwonly <ysjjw2003@naver.com>
2025-10-31 23:44:27 +0900

    fix: apply coderabbit comment

--- src/main/java/finity/fini/config/WebConfig.java
+++ src/main/java/finity/fini/config/WebConfig.java
@@ -23,8 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-//                .allowedOrigins("https://your-production-frontend.com")
-                .allowedOriginPatterns("http://localhost:3000")
+                .allowedOriginPatterns("http://localhost:3000", "https://*.finiapp.tech")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .exposedHeaders("Authorization", "Content-Type");
